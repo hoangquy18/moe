9b2041d8ec1675d52ebf910641c51d954f31dcdb
Nguyen Hoang Quy <20521815@gm.uit.edu.vn>
2025-08-09 07:32:02 +0700

    Update

--- model/mm/mm_encoder.py
+++ model/mm/mm_encoder.py
@@ -20,61 +20,12 @@ class MultiModalEncoder(nn.Module):
             for param in self.vision_encoder.parameters():
                 param.requires_grad = False
 
-        self.text_projection = nn.Linear(
-            self.text_encoder.text_config.hidden_size, config.hidden_size
-        )
-        self.vision_projection = nn.Linear(
-            self.vision_encoder.vision_config.hidden_size, config.hidden_size
-        )
-
-        self.mm_encoder_layer = nn.TransformerEncoderLayer(
-            d_model=config.hidden_size, nhead=config.num_attention_heads
-        )
-        self.mm_encoder = nn.TransformerEncoder(
-            self.mm_encoder_layer, num_layers=config.num_encoder_layers
-        )
-
-        self.text_layernorm = nn.LayerNorm(
-            config.hidden_size, eps=config.layer_norm_eps
-        )
-        self.vision_layernorm = nn.LayerNorm(
-            self.vision_encoder.vision_config.hidden_size, eps=config.layer_norm_eps
-        )
         self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
         self.logit_bias = nn.Parameter(torch.ones([]) * -10)
 
         self.proj_type = config.proj_type
         if self.proj_type == "map":
             self.map_head = MultiheadAttentionPoolingHead(config)
-        self.initialize_parameters()
-
-    def initialize_parameters(self):
-        nn.init.xavier_uniform_(self.text_projection.weight)
-        nn.init.xavier_uniform_(self.vision_projection.weight)
-        nn.init.constant_(self.text_projection.bias, 0)
-        nn.init.constant_(self.vision_projection.bias, 0)
-
-        nn.init.normal_(self.text_layernorm.weight, std=0.02)
-        nn.init.normal_(self.text_layernorm.bias, std=0.02)
-        nn.init.normal_(self.vision_layernorm.weight, std=0.02)
-        nn.init.normal_(self.vision_layernorm.bias, std=0.02)
-
-        if self.proj_type == "map":
-            self.map_head.initialize_parameters()
-
-    def project_text_features(self, text_features: torch.Tensor) -> torch.Tensor:
-        residual = text_features
-        text_features = self.text_layernorm(text_features)
-        text_features = self.text_projection(text_features)
-        text_features = residual + text_features
-        return text_features
-
-    def project_image_features(self, image_features: torch.Tensor) -> torch.Tensor:
-        residual = image_features
-        image_features = self.vision_layernorm(image_features)
-        image_features = self.vision_projection(image_features)
-        image_features = residual + image_features
-        return image_features
 
     def feature_extraction(
         self,
@@ -122,30 +73,6 @@ class MultiModalEncoder(nn.Module):
             "logit_bias": self.logit_bias,
         }
 
-    def get_multimodal_embeddings(self, text_features, image_features):
-        """
-        Get combined multimodal embeddings from text and image features.
-        Useful for downstream tasks that need a single representation.
-
-        Args:
-            text_features: Text features from the text encoder
-            image_features: Image features from the vision encoder
-
-        Returns:
-            Combined multimodal embedding
-        """
-        # Project features to the same space
-        text_projection = self.project_text_features(text_features)
-        image_projection = self.project_image_features(image_features)
-
-        # Average pooling for a simple fusion
-        multimodal_embedding = (text_projection.mean(dim=1) + image_projection.mean(dim=1)) / 2
-
-        # Normalize the embedding
-        multimodal_embedding = multimodal_embedding / multimodal_embedding.norm(dim=1, keepdim=True)
-
-        return multimodal_embedding
-
     def forward(
         self,
         text_input_ids,
@@ -184,23 +111,9 @@ class MultiModalEncoder(nn.Module):
 
         image_features = self.vision_encoder(image_features)
 
-        B, text_len, _ = text_features.size()
-
-        text_projection = self.project_text_features(text_features)
-        image_projection = self.project_image_features(image_features)
-
-        text_image_features = torch.cat((text_projection, image_projection), dim=1)
-        mm_features = self.mm_encoder(text_image_features)
-
-        mm_images = mm_features[:, text_len:, :]
-        mm_texts = mm_features[:, :text_len, :]
-
-        mm_images = self.feature_extraction(mm_images, self.proj_type)
-        mm_texts = self.feature_extraction(mm_texts, self.proj_type)
-
         # normalized features
-        mm_images = mm_images / mm_images.norm(dim=1, keepdim=True)
-        mm_texts = mm_texts / mm_texts.norm(dim=1, keepdim=True)
+        mm_images = image_features / image_features.norm(dim=1, keepdim=True)
+        mm_texts = text_features / text_features.norm(dim=1, keepdim=True)
 
         if return_embeddings_only:
             return mm_texts, mm_images
